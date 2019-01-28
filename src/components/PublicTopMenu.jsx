062f25bd94f03a58f892e5327776e61ab77f80d7
Sergiy Koyev <you@example.com>
2019-01-27 19:15:23 -0500

    Updated

--- src/components/PublicTopMenu.jsx
+++ src/components/PublicTopMenu.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
-const PublicTopMenu = ({name}) => (
- <div>{`Hi ${name}`}</div>
+const PublicTopMenu = ({name}) => ( 
+ <section role="navigation" class="navbar">
+    <nav class="navbar navbar-light bg-light">
+        <a class="nav-link" href="#">Link1</a>
+        <a class="nav-link" href="#">Link2</a>
+        <a class="nav-link" href="#">Link3</a>
+        <a class="nav-link" href="#">Link4</a>
+        <a class="nav-link" href="#">Link5</a>
+        <a class="nav-link" href="#">Link6</a>
+        <a class="nav-link" href="#">Link7</a>
+    </nav>
+ </section>
 );
 
 export default PublicTopMenu;
