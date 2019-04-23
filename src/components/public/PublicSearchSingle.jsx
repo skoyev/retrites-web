d357d4b2566bb527f8c6d7144383593a2acee3ed
Sergiy Koyev <skoyev@gmail.com>
2019-04-23 15:40:05 -0400

    Development

--- src/components/public/PublicSearchSingle.jsx
+++ src/components/public/PublicSearchSingle.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './style/PublicSearch.css'
 import { Translate } from "react-localize-redux";
+import { Icon, Button, Input, DatePicker, Select } from "antd";
+import "antd/dist/antd.css";
 
 class PublicSearchSingle extends React.Component {
 
@@ -16,12 +18,21 @@ class PublicSearchSingle extends React.Component {
 
     render() {
         //let {name, menuItems} = this.props;
+        const Search = Input.Search;
+        const InputGroup = Input.Group;
+        const Option = Select.Option;
+        const { MonthPicker, RangePicker, WeekPicker } = DatePicker;
+
         return (
         <div className="slider-section">
-            <h2>Search</h2>
             <section className="search-witget" role="navigation">        
-                <div className="tab-content search-witget-content">
-                    Content
+                <div className="tab-content search-witget-content"> 
+                    <h3>Find Retreates For Any Season</h3>
+                    <InputGroup compact>
+                        <Input style={{ width: '40%', height: '35px' }} placeholder="Where would you like to go ?"/>
+                        <RangePicker style={{ width: '30%', marginLeft: '5px', height: '35px' }} onChange={console.log()} />
+                        <Button style={{ marginLeft: '5px', borderRadius: '5px', height: '35px' }} type="primary" icon="search">Search</Button>
+                    </InputGroup>                                   
                 </div>        
             </section>
         </div>        
