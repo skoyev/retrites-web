e2aeed46f5e45566ae7e1ff6d94c5a541a38f9e1
Sergiy Koyev <skoev@hotmail.com>
2021-04-27 17:05:22 -0400

    Dev

--- src/components/private/message/Message.jsx
+++ src/components/private/message/Message.jsx
@@ -19,106 +19,107 @@ const rowSelection = {
     name: record.name,
   }),
 };
-  
+
 const Message = props => {
-    let timer;  
-    const {states, messageGroups, handleViewMessage, handleDeleteMessage, messagePageNum, user} = props;  
+  let timer;
+  const { states, messageGroups, handleViewMessage, handleDeleteMessage, messagePageNum, user } = props;
 
-    const [name, setName] = useState('');
-    const [selectedState, setSelectedState] = useState({});   
-    const [pageNum, setPageNum] = useState(0);   
-    const getRecipientName = (record) => {
-      return user.firstName == record.recipient.firstName && 
-              user.lastName == record.recipient.lastName ? 
-                `${record.owner.firstName} ${record.owner.lastName}` :
-                `${record.recipient.firstName} ${record.recipient.lastName}`;
+  const [name, setName] = useState('');
+  const [selectedState, setSelectedState] = useState({});
+  const [pageNum, setPageNum] = useState(0);
+  const getRecipientName = (record) => {
+    return user.firstName == record.recipient.firstName &&
+      user.lastName == record.recipient.lastName ?
+      `${record.owner.firstName} ${record.owner.lastName}` :
+      `${record.recipient.firstName} ${record.recipient.lastName}`;
+  }
+
+  const columns = [
+    {
+      title: 'Recipient Name',
+      dataIndex: '',
+      render: record => <a>{getRecipientName(record)}</a>,
+    },
+    {
+      title: 'Created Date',
+      dataIndex: 'createdAt',
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (text, record) => (
+        <Row>
+          <Col span={6}>
+            <Button key={record.id} type="link" onClick={() => handleViewMessage({ id: record.id, pageNum: pageNum, recipient: getRecipientName(record), itemID: record.item.id })}>View</Button>
+          </Col>
+          <Col span={6}>
+            <Button key={record.id} type="link" onClick={() => handleDeleteMessage(record.id, getRecipientName(record))}>Delete</Button>
+          </Col>
+        </Row>
+      )
     }
+  ];
 
-    const columns = [
-      {
-        title: 'Recipient Name',
-        dataIndex: '',
-        render: record => <a>{getRecipientName(record)}</a>,
-      },
-      {
-        title: 'Created Date',
-        dataIndex: 'createdAt',
-      },
-      {
-        title: 'Action',
-        key: 'action',
-        render: (text, record) => (
-          <Row>
-            <Col span={6}>
-              <Button key={record.id} type="link" onClick={() => handleViewMessage({id:record.id, pageNum:pageNum, recipient:getRecipientName(record), itemID: record.item.id})}>View</Button>
-            </Col>
-            <Col span={6}>
-              <Button key={record.id} type="link" onClick={() => handleDeleteMessage(record.id, getRecipientName(record))}>Delete</Button>
-            </Col>
-          </Row>
-        )
-      }
-    ];    
+  let onChangeDebounced = () => {
+    props.fetchMessageGroups();
+  };
 
-    let onChangeDebounced = () => {
-        //props.fetchMessageGroups();
-    };
-                      
-    useEffect(()=> {
-        props.fetchMessageGroups();
-    }, []);
+  useEffect(() => {
+    const interval = setInterval(() => props.fetchMessageGroups(), WAIT_INTERVAL);
+    return () => clearInterval(interval)
+  }, []);
 
-    useEffect(() => {
-        clearTimeout(timer);
-        timer = setTimeout(onChangeDebounced, WAIT_INTERVAL);
-    }, [selectedState, name]);    
+  useEffect(() => {
+    //clearTimeout(timer);
+    //timer = setTimeout(onChangeDebounced, WAIT_INTERVAL);
+  }, [selectedState, name]);
 
-    return (
-        <Fragment>
-            <Row className="msg-header">
-              <Col span={2} className="label">
-                <Translate>{({ translate }) => <span>{translate("label.name")}</span>}</Translate>                                        
-              </Col>
-              <Col span={3}>
-                <Input onChange={(v) => setName(v.target.value)}/>
-              </Col>
-              <Col span={2} className="label">
-                <Translate>{({ translate }) => <span>{translate("label.status")}</span>}</Translate>
-              </Col>
-              <Col span={2}>
-                <Dropdown.Button id="type" overlay={typeMenu(states, (v) => setSelectedState(v.item.props.data[v.key]))}>
-                    <span>{selectedState.name}</span>
-                </Dropdown.Button>
-              </Col>
-            </Row>
-            
-            <Row>
-              <Table rowSelection={rowSelection} 
-                     columns={columns} 
-                     key="messages"
-                     rowKey="id"
-                     className="message"
-                     onChange={(page, pageSize) => {setPageNum(page.current)}}
-                     pagination={{defaultPageSize:5, hideOnSinglePage: true, simple: true, defaultCurrent:messagePageNum}}
-                     dataSource={messageGroups} />
-            </Row>
-        </Fragment>
-    )
+  return (
+    <Fragment>
+      <Row className="msg-header">
+        <Col span={2} className="label">
+          <Translate>{({ translate }) => <span>{translate("label.name")}</span>}</Translate>
+        </Col>
+        <Col span={3}>
+          <Input onChange={(v) => setName(v.target.value)} />
+        </Col>
+        <Col span={2} className="label">
+          <Translate>{({ translate }) => <span>{translate("label.status")}</span>}</Translate>
+        </Col>
+        <Col span={2}>
+          <Dropdown.Button id="type" overlay={typeMenu(states, (v) => setSelectedState(v.item.props.data[v.key]))}>
+            <span>{selectedState.name}</span>
+          </Dropdown.Button>
+        </Col>
+      </Row>
+
+      <Row>
+        <Table rowSelection={rowSelection}
+          columns={columns}
+          key="messages"
+          rowKey="id"
+          className="message"
+          onChange={(page, pageSize) => { setPageNum(page.current) }}
+          pagination={{ defaultPageSize: 5, hideOnSinglePage: true, simple: true, defaultCurrent: messagePageNum }}
+          dataSource={messageGroups} />
+      </Row>
+    </Fragment>
+  )
 }
 
-Message.propTypes = {  
+Message.propTypes = {
 }
 
-const mapDispatchToProps = { 
+const mapDispatchToProps = {
   ...commonActions,
   ...messageActions
-}; 
+};
 
 function mapStateToProps(state) {
   return {
-      states  : state.common.messageStates,
-      messageGroups: state.message.messageGroups,
-      user : JSON.parse(sessionStorage.getItem('user'))
+    states: state.common.messageStates,
+    messageGroups: state.message.messageGroups,
+    user: JSON.parse(sessionStorage.getItem('user'))
   };
 }
 
--- src/components/private/message/details/MessageDetails.jsx
+++ src/components/private/message/details/MessageDetails.jsx
@@ -9,153 +9,155 @@ import './index.css'
 import { Loader } from '../../../common';
 import moment from 'moment';
 import RetreatPhotoAlbum from '../../../public/retreat/RetreatPhotoAlbum';
-  
+
 const MessageDetails = props => {
-  
-    const columns = [
-      {
-        key: 'action1',
-        render: (text, record) => (
+
+  const columns = [
+    {
+      key: 'action1',
+      render: (text, record) => (
+        <Row>
+          <Col span={3}>
+            <img style={{ borderRadius: '50%', width: 35 }} src="https://a0.muscache.com/defaults/user_pic-225x225.png?v=3" />
+          </Col>
+        </Row>
+      )
+    },
+    {
+      title: (text, record) => <Row><Col span={24}></Col></Row>,
+      className: ['wide', 'center'],
+      key: 'action2',
+      render: (text, record) => (
+        <>
           <Row>
-            <Col span={3}>
-              <img style={{borderRadius:'50%', width: 35}} src="https://a0.muscache.com/defaults/user_pic-225x225.png?v=3"/>
+            <Col span={12} style={{ textAlign: 'left' }}>
+              <b>From: </b>{`${record.owner.firstName} ${record.owner.lastName}`}
+            </Col>
+            <Col span={12} style={{ textAlign: 'right' }}>
+              <span style={{ marginLeft: 10 }}><b>Date: </b>{moment(record.createdAt).format('DD-MM-YYYY HH:mm')}</span>
             </Col>
           </Row>
-        )
-      },
-      {
-        title: (text, record) => <Row><Col span={24}>Details</Col></Row>,
-        className: ['wide', 'center'],
-        key: 'action2',
-        render: (text, record) => (
-          <>
-            <Row>
-              <Col style={{textAlign:'left'}}>
-                <b>{`${record.owner.firstName} ${record.owner.lastName}`}</b>
-                <span style={{marginLeft:10}}><b>{moment(record.createdAt).format('DD-MM-YYYY HH:mm')}</b></span>
-              </Col>
-            </Row>
 
-            <Row>
-              <Col span={6} style={{textAlign:'left'}}>
-                {record.content}
-              </Col>
-            </Row>
-          </>
-        )
-      }
-    ];
+          <Row>
+            <Col span={6} style={{ textAlign: 'left' }}>
+              <b>Message: </b>{record.content}
+            </Col>
+          </Row>
+        </>
+      )
+    }
+  ];
 
-    useEffect(()=> {
-        // initial data load
-        props.fetchMessagesByGroupID(props.messageGroupId);
+  useEffect(() => {
+    // initial data load
+    props.fetchMessagesByGroupID(props.messageGroupId);
 
-        if(props.itemID && (typeof parseInt(props.itemID) === "number")){
-          // load item details
-          props.fetchByID(parseInt(itemID));
-        }
+    if (props.itemID && (typeof parseInt(props.itemID) === "number")) {
+      // load item details
+      props.fetchByID(parseInt(itemID));
+    }
 
-        // update every 1.5 min
-        const interval = setInterval(() => 
-          props.fetchMessagesByGroupID(props.messageGroupId), 5000);
+    // update every 1.5 min
+    const interval = setInterval(() =>
+      props.fetchMessagesByGroupID(props.messageGroupId), 5000);
 
-        return () => {
-          clearInterval(interval);
-        };
-    }, []);
-
-    const [message, setMessage] = useState('');
-    const reply = () => {
-      if(message && message.length > 0){
-        setMessage(''); // clear messages
-        props.createMessage(message, props.messageGroupId);        
-      }      
+    return () => {
+      clearInterval(interval);
     };
+  }, []);
 
-    const footerSection = () => 
-      <Row>
-        <Col span={4}>
-          <span>Reply Message:</span>
-        </Col>       
+  const [message, setMessage] = useState('');
+  const reply = () => {
+    if (message && message.length > 0) {
+      setMessage(''); // clear messages
+      props.createMessage(message, props.messageGroupId);
+    }
+  };
 
-        <Col span={12}>
-          <Input placeholder="Type Your Message Here" value={message} onChange={(e) => setMessage(e.target.value)}/>
-        </Col>       
+  const footerSection = () =>
+    <Row>
+      <Col span={4} style={{ paddingTop: 5 }}>
+        <span>Reply Message:</span>
+      </Col>
 
-        <Col span={2} offset={1}>
-          <Button onClick={() => reply()}>Send</Button>
-        </Col> 
-      </Row>
+      <Col span={12}>
+        <Input placeholder="Type Your Message Here" value={message} onChange={(e) => setMessage(e.target.value)} />
+      </Col>
 
-    const headerSection = () => 
-      <Row>
-        <Col span={24} className="center">
-            <span>Communication With: <b>{props.recipient}</b></span>
+      <Col span={2} offset={1}>
+        <Button onClick={() => reply()}>Send</Button>
+      </Col>
+    </Row>
+
+  const headerSection = () =>
+    <Row>
+      <Col span={24} style={{ textAlign: 'left' }}>
+        <span><b>Communication With: </b>{props.recipient}</span>
+      </Col>
+    </Row>
+
+  const { isLoading, itemID, item } = props;
+
+  return (
+    <Fragment>
+      <Row className="msg-header">
+        <Col span={2} className="label">
+          <Translate>{({ translate }) => <Button onClick={() => props.backToMessageFromDetails()}>{translate("label.back")}</Button>}</Translate>
         </Col>
       </Row>
-
-    const {isLoading, itemID, item} = props;
-
-    return (
-        <Fragment>
-            <Row className="msg-header">
-              <Col span={2} className="label">
-                <Translate>{({ translate }) => <Button onClick={() => props.backToMessageFromDetails()}>{translate("label.back")}</Button>}</Translate>
-              </Col>
-            </Row>
-            <Row>
-                <Col span={16}>
-                  <Table
-                        columns={columns} 
-                        key="message-details"
-                        rowKey="id"
-                        title={ headerSection }
-                        footer={ footerSection }
-                        pagination={{defaultPageSize:5, hideOnSinglePage: true, simple: true}}
-                        dataSource={props.messageDetails} />
+      <Row>
+        <Col span={16}>
+          <Table
+            columns={columns}
+            key="message-details"
+            rowKey="id"
+            title={headerSection}
+            footer={footerSection}
+            pagination={{ defaultPageSize: 5, hideOnSinglePage: true, simple: true }}
+            dataSource={props.messageDetails} />
+        </Col>
+        <Col span={8}>
+          {item
+            &&
+            <>
+              <Row>
+                <Col span={24}>
+                  <img src={item ? item.picture : ''} style={{ width: '90%' }} />
                 </Col>
-                <Col span={8}>
-                  { item 
-                      &&
-                    <>
-                      <Row>
-                        <Col span={24}>
-                          <img src={item ? item.picture : ''} style={{width:'90%'}}/>
-                        </Col>
-                      </Row>
-                      <Row>
-                        <Col className="center">
-                          <h3>{item.name}</h3>
-                        </Col>
-                      </Row>
-                      <Row>
-                        <Col className="left">
-                          <h4>{`Description: ${item.description}`}</h4>
-                        </Col>
-                      </Row>
-                    </>
-                  }
-                </Col>             
-            </Row>
-            {
-                isLoading && <Loader text=""/>
-            }
-        </Fragment>
-    )
+              </Row>
+              <Row>
+                <Col className="center">
+                  <h3>{item.name}</h3>
+                </Col>
+              </Row>
+              <Row>
+                <Col className="left">
+                  <h4>{`Description: ${item.description}`}</h4>
+                </Col>
+              </Row>
+            </>
+          }
+        </Col>
+      </Row>
+      {
+        isLoading && <Loader text="" />
+      }
+    </Fragment>
+  )
 }
 
-const mapDispatchToProps = { 
+const mapDispatchToProps = {
   ...itemActions,
   ...commonActions,
   ...messageActions
-}; 
+};
 
 function mapStateToProps(state) {
   return {
-      states  : state.common.messageStates,
-      messageDetails: state.message.messages,
-      isLoading: state.common.isLoading,
-      item: state.items.item
+    states: state.common.messageStates,
+    messageDetails: state.message.messages,
+    isLoading: state.common.isLoading,
+    item: state.items.item
   };
 }
 
